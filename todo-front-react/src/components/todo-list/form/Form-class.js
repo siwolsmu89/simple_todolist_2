54083bdcdac55ec6a4c1ad5808c699c598937903
siwolsmu89 <siwol_smuire89@naver.com>
2021-04-20 14:15:04 +0900

    Modify Form component as class style / Create 2 files(Form-class.js, Form-function.js) to compare differences 2021/04/20

--- todo-front-react/src/components/todo-list/form/Form-class.js
+++ todo-front-react/src/components/todo-list/form/Form-class.js
@@ -3,6 +3,8 @@ import './Form.css';
 import { addTodo } from "../../actions/todoActions";
 import { connect } from "react-redux";
 
+// Form 컴포넌트 component class 형태로 구현했을 때 (함수 형태로 구현했을 때와 기능은 완전히 동일)
+
 // smart component + dumb component
 // 액션 처리 및 화면 표시를 함께 담당
 // 원래라면 화면 표시를 위한 dumb 컴포넌트를 별도로 분리해야 하지만, 이미 작은 컴포넌트이므로 화면 표시까지 smart component 내에서 처리
---
+++ todo-front-react/src/components/todo-list/form/Form-function.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Form.css';
+import { addTodo } from "../../actions/todoActions";
+import { connect } from "react-redux";
+
+// Form 컴포넌트 함수 형태로 구현했을 때 (class 형태로 구현했을 때와 기능은 완전히 동일)
+
+// smart component + dumb component
+// 액션 처리 및 화면 표시를 함께 담당
+// 원래라면 화면 표시를 위한 dumb 컴포넌트를 별도로 분리해야 하지만, 이미 작은 컴포넌트이므로 화면 표시까지 smart component 내에서 처리
+let Form = ({ dispatch}) => {
+    // 변하는 값이므로 let
+    let input
+
+    const onKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            onCreate();
+        }
+    }
+
+    // 액션 생성자의 addTodo() 함수를 호출하여 액션을 생성하고 dispatch() 함수로 스토어에 액션을 전달
+    const onCreate = () => {
+        dispatch(addTodo(input.value));
+        input.value = '';
+    }
+
+    return (
+        <div className="form">
+            <input ref={node => { input = node }} onKeyPress={onKeyPress} />
+            <div className="create-button" onClick={onCreate}>
+                추가
+            </div>
+        </div>
+    );
+};
+
+// 스토어에 연결
+Form = connect()(Form);
+
+export default Form;
